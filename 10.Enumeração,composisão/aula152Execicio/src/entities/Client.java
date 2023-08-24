7299b554f8503658ce113f4bb7c618e66b96c0f3
Douglas <127360583+privateclasswizard@users.noreply.github.com>
2023-08-24 14:44:09 -0300

    Add files via upload

---
+++ 10.Enumeração,composisão/aula152Execicio/src/entities/Client.java
@@ -0,0 +1,37 @@
+package entities;
+
+import java.util.Date;
+
+public class Client {
+	private String nome;
+	private String email;
+	private Date birthDate;
+	
+	public Client(String nome, String email, Date birthDate) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.birthDate = birthDate;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	
+}
---
+++ 10.Enumeração,composisão/aula152Execicio/src/enumsEntities/OrderStatus.java
@@ -0,0 +1,8 @@
+package enumsEntities;
+
+public enum OrderStatus {
+	PENDINIG_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+}
